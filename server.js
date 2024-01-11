17c97e2bbe5862ca0e5db925eed0fe2bf1055d9a
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-11 11:11:46 -0500

    require handlebars in server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars ");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () =>
-  console.log(`Now Listening`)
-);
+app.listen(PORT, () => console.log(`Now Listening`));
