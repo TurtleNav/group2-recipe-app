58929592b97d4a4ba5008138eb491d6512734ae3
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-11 12:22:33 -0500

    fixed homepage route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars ");
+app.set("view engine", "handlebars");
 
 const sess = {
   secret: "Super secret secret",
