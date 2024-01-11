2382b2cdab605fb851cd942471de498a87ec4659
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-11 08:41:29 -0800

    Add model for recipe databse

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recipe_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    has_nuts: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Recipe',
+  }
+);
+
+module.exports = Recipe;
