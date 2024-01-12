65213473b79b07cea5e6bda2ca1c04ee4ebfe12b
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-12 10:18:36 -0800

    Revise structure of fetch request to save nutrient information as an array of objects

--- fetchRecipe.js
+++ fetchRecipe.js
@@ -1,3 +1,9 @@
+const apiKey = '8d3a74b58a844dd0880bad6638d783d0'; // Replace with your actual API key
+
+const userSearch = 'pasta';
+const maxCalories = 700;
+const numberOfResults = 5;
+
 const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${userSearch}&maxCalories=${maxCalories}&number=${numberOfResults}&apiKey=${apiKey}`;
 
 let fetchedData = [];
