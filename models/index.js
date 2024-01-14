74e7484aec4953a3c174546d069c8c27c319c13d
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-14 12:38:01 -0500

    Fixed missing import - recipe seeding works now

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const Ingredient = require('./Ingredient');
 const User = require('./User');
+const Recipe = require('./Recipe');
 
-module.exports = { Ingredient, User };
+module.exports = { Ingredient, User, Recipe };
