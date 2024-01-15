7f377c722e7288be3db0ad06f4746b759cd784c0
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-14 23:30:40 -0500

    Added get route for recipes by ID

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,11 +1,32 @@
 const router = require('express').Router();
 const Recipe = require('../../models/Recipe');
 
+/*
+  Get route for all cached Recipes
+*/
 router.get('/', async (req, res) => {
-  const recipeData = await Recipe.findAll();
-
-  res.json(recipeData);
+  try {
+    const recipeData = await Recipe.findAll();
+    res.status(200).json(recipeData);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
+
+/*
+  Get recipe by id
+*/
+router.get('/:id', async (req, res) => {
+  try {
+    const recipeData = await Recipe.findByPk(req.params.id);
+    res.status(200).json(recipeData);
+  } catch(err) {
+    console.error(err);
+    res.status(404).json(err);
+  }
+});
+
 
 router.post('/', async (req, res) => {
   const newRecipeData = await Recipe.create(req.body);
