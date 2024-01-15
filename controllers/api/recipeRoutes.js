61dfb9c753f4959a2fb58584b8bc5dd90837382a
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-15 12:26:41 -0500

    Fleshed out the get recipe route to handle query parameters. Currently calorie count is the only implemented query parameter

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,12 +1,40 @@
 const router = require('express').Router();
 const Recipe = require('../../models/Recipe');
 
+const {Op} = require('sequelize');
+
+
 /*
   Get route for all cached Recipes
+
+  query parameters:
+  minCalories
+  maxCalories
+
 */
 router.get('/', async (req, res) => {
   try {
-    const recipeData = await Recipe.findAll();
+    const options = { where: {} };
+
+    // Parse out integer values for both minCalories and maxCalories.
+    // If properly formatted integers then the values will be used to filter
+    // results based on calorie count
+    const minCalories = parseInt(req.query.minCalories);
+    const maxCalories = parseInt(req.query.maxCalories);
+
+    if (minCalories || maxCalories) {
+      const calories = {};
+
+      if (minCalories) {
+        Object.assign(calories, {[Op.gte]: minCalories});
+      }
+      if (maxCalories) {
+        Object.assign(calories, {[Op.lte]: maxCalories});
+      }
+      options.where.calories = calories;
+    }
+
+    const recipeData = await Recipe.findAll(options);
     res.status(200).json(recipeData);
   } catch(err) {
     console.error(err);
@@ -26,6 +54,22 @@ router.get('/:id', async (req, res) => {
     res.status(404).json(err);
   }
 });
+
+/*
+  Get recipe by calorie content
+
+router.get('/:calories?', async (req, res) => {
+  try {
+    console.log(req.params.calories);
+    const recipeData = await Recipe.findAll();
+    res.status(200).json(recipeData);
+  } catch(err) {
+    console.error(err);
+    res.status(404).json(err);
+  }
+});
+
+*/
 
 
 router.post('/', async (req, res) => {
