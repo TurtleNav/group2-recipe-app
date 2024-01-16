40234911ad8f074c1ed7366ee8da3203666ae82d
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-16 11:19:09 -0500

    Structured data request using a map function to pull recipe ID's

--- fetchIngredients.js
+++ fetchIngredients.js
@@ -1,7 +1,7 @@
 const apiKey = '8d3a74b58a844dd0880bad6638d783d0'; // Replace with your actual API key
 
 const ingredientsList = 'apples,flour,sugar';
-const numberOfRecipes = 2;
+const numberOfRecipes = 1;
 const ranking = 1;
 
 const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsList}&number=${numberOfRecipes}&ranking=${ranking}&apiKey=${apiKey}`;
@@ -9,7 +9,14 @@ const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredient
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-     console.log(data);
+    console.log(data);
+    return data; // Returning data for further use
+  })
+  .then(data => {
+    const RecipeInfo = data.map(recipe => ({
+      id: recipe.id,
+    }));
+    console.log(RecipeInfo);
   })
   .catch(error => {
     console.error('Error fetching data:', error);
