4da02e02fd17a0d1d744d91d973c804c4492bf93
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-15 23:07:13 -0500

    Recipes can now be queried based on carbs/fat/protein content

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -3,6 +3,14 @@ const Recipe = require('../../models/Recipe');
 
 const {Op} = require('sequelize');
 
+// Map object that maps a SQL model's name to query parameters
+const queryParams = new Map([
+  ['calories', ['minCalories', 'maxCalories']],
+  ['carbs', ['minCarbs', 'maxCarbs']],
+  ['fat', ['minFat', 'maxFat']],
+  ['protein', ['minProtein', 'maxProtein']]
+]);
+
 
 /*
   Get route for all cached Recipes
@@ -20,33 +28,36 @@ const {Op} = require('sequelize');
 
   minProtein
   maxProtein
-
-
 */
 router.get('/', async (req, res) => {
   try {
-    const options = { where: {} };
+    const options = { where: {}};
 
-    // Parse out integer values for both minCalories and maxCalories.
-    // If properly formatted integers then the values will be used to filter
-    // results based on calorie count
-    const minCalories = parseInt(req.query.minCalories);
-    const maxCalories = parseInt(req.query.maxCalories);
+    // params is the array of values in the queryParams map
+    // family is a key in the queryParams map
+    queryParams.forEach((params, family) => {
+      let queryObj;
+      for (const param of params) {
+        if (Object.hasOwn(req.query, param)) {
+          const paramValue = parseInt(req.query[param]);
 
-    if (minCalories || maxCalories) {
-      const calories = {};
+          if (paramValue) {
+            if (!queryObj) {
+              // Create an object for the query rules to be added to the 'options' object
+              options.where[family] = queryObj = {};
+            }
 
-      if (minCalories) {
-        Object.assign(calories, {[Op.gte]: minCalories});
+            if (param.startsWith('min')) {
+              Object.assign(queryObj, {[Op.gte]: paramValue});
+            } else if (param.startsWith('max')) {
+              Object.assign(queryObj, {[Op.lte]: paramValue});
+            } else {
+              throw new Error('Invalid query parameter');
+            }
+          }
+        }
       }
-      if (maxCalories) {
-        Object.assign(calories, {[Op.lte]: maxCalories});
-      }
-      options.where.calories = calories;
-    }
-
-    //if (req.query.minProtein)
-
+    });
     const recipeData = await Recipe.findAll(options);
     res.status(200).json(recipeData);
   } catch(err) {
@@ -67,23 +78,6 @@ router.get('/:id', async (req, res) => {
     res.status(404).json(err);
   }
 });
-
-/*
-  Get recipe by calorie content
-
-router.get('/:calories?', async (req, res) => {
-  try {
-    console.log(req.params.calories);
-    const recipeData = await Recipe.findAll();
-    res.status(200).json(recipeData);
-  } catch(err) {
-    console.error(err);
-    res.status(404).json(err);
-  }
-});
-
-*/
-
 
 router.post('/', async (req, res) => {
   const newRecipeData = await Recipe.create(req.body);
