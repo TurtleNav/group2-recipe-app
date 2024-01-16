1be93b70f812787a1c00f46ff772ead128a63a15
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-16 11:09:50 -0500

    adding ranking parameter

--- fetchIngredients.js
+++ fetchIngredients.js
@@ -2,14 +2,14 @@ const apiKey = '8d3a74b58a844dd0880bad6638d783d0'; // Replace with your actual A
 
 const ingredientsList = 'apples,flour,sugar';
 const numberOfRecipes = 2;
+const ranking = 1;
 
-const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsList}&number=${numberOfRecipes}&apiKey=${apiKey}`;
+const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsList}&number=${numberOfRecipes}&ranking=${ranking}&apiKey=${apiKey}`;
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    // Process the response data here
-    console.log(data);
+     console.log(data);
   })
   .catch(error => {
     console.error('Error fetching data:', error);
