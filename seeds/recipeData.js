7e74e24d5cbb5a20ddd5bb8b190cc7a3f1409956
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-16 13:32:08 -0500

    added 'sugar' and removed superfluous single quotes in recipe model seed data

--- seeds/recipeData.js
+++ seeds/recipeData.js
@@ -2,20 +2,26 @@ const {Recipe} = require('../models');
 
 const recipeData = [
   {
-    'title': 'Baked Apples in White Wine',
-    'id': 90629,
-    'calories': 210,
-    'carbs': 43,
-    'fat': 3,
-    'protein': 1
+    title: 'Baked Apples in White Wine',
+    id: 90629,
+    calories: 210,
+    carbs: 43,
+    fat: 3,
+    protein: 1,
+    sodium: 154.2,
+    cholesterol: 27.8,
+    sugar: 34
   },
   {
-    'title': 'Chocolate Silk Pie with Marshmallow Meringue',
-    'id': 284420,
-    'calories': 226,
-    'carbs': 33,
-    'fat': 10,
-    'protein': 2
+    title: 'Chocolate Silk Pie with Marshmallow Meringue',
+    id: 284420,
+    calories: 226,
+    carbs: 33,
+    fat: 10,
+    protein: 2,
+    sodium: 389.9,
+    cholesterol: 90.4,
+    sugar: 27.2
   }
 ];
 
