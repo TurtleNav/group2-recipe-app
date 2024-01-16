f7a688ef04adb1a35976289ed5997ab21ac28394
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-16 14:55:18 -0500

    Deleted old code

--- fetchRecipe.js
+++ fetchRecipe.js
@@ -39,22 +39,4 @@ fetch(apiUrl)
 
 
 
-    //   // Iterate through nutrients array and log each nutrient
-    //   // recipe.nutrition.nutrients.forEach(nutrient => {
-    //   //   console.log(`Nutrient: ${nutrient.title}, Amount: ${nutrient.amount}${nutrient.unit}`);
-    //   //   recipeInfo.nutrition.nutrients.push({
-    //   //     title: nutrient.title,
-    //   //     amount: nutrient.amount,
-    //   //     unit: nutrient.unit
-    //   //   });
-    //   // });
 
-    //   nutritionInformation.push(recipeInfo);
-    // });
-
-    // Now, the nutritionInformation array contains the nutrition information for each recipe
-  //   console.log(nutritionInformation);
-  // })
-  // .catch(error => {
-  //   console.error('Error fetching data:', error);
-  // });
