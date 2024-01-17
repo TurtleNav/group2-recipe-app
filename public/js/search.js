eecd552936207b6ff16ed24e42b727cfde1b3e47
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-17 16:59:31 -0500

    Have a bare-bones search page that will console log a mock search

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,4 @@
-/* File for handling the search form in the affiliated search.handlebars file
+/* File for handling the recipe search form in the affiliated search.handlebars file
 
   NOTES:
   -----
@@ -6,8 +6,37 @@
       rule to decrease font size with decreasing window size
 
 */
-const searchSelectEl = document.getElementById('search-method');
+const searchSelect = document.getElementById('search-method');
+const searchForm = document.getElementById('search-form');
 
-searchSelectEl.addEventListener('change', () => {
-  console.log(`hey you clicked on the ${searchSelectEl.selectedIndex}`);
+// Global variable representing the recipe search method
+// 1: searchByIngredient | 2: searchByNutrient
+let searchMethod;
+
+console.log(searchForm);
+
+// search method when 'Search By Ingredient' is selected
+function searchByIngredient() {
+  console.log('searching by ingredients...');
+}
+
+// search method when 'Search By Nutrient' is selected
+function searchByNutrient() {
+  console.log('searching by nutrients...');
+}
+
+// Whenever an option is selected, reassign the global searchMethod variable
+searchSelect.addEventListener('change', () => {
+  searchMethod = searchSelect.selectedIndex;
+});
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (searchMethod === 1) {
+    searchByIngredient();
+  } else if (searchMethod === 2) {
+    searchByNutrient();
+  } else {
+    console.log('A search method needs to be selected before you can search');
+  }
 });
