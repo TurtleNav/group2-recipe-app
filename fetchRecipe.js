a75084d5e95003794de22c94f94e087750d9ab09
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-17 12:05:11 -0500

    Add fetch request files

--- fetchRecipe.js
+++ fetchRecipe.js
@@ -1,35 +1,38 @@
 require('dotenv').config();
-const apiKey = process.env.API_KEY;
+const fetch = require('node-fetch');
 
-const userSearch = 'pasta';
-const maxCalories = 700;
-const minProtein = 20;
-const numberOfResults = 1;
+function fetchRecipesWithParameters(userSearch, maxCalories, numberOfResults) {
+  const apiKey = process.env.API_KEY;
+  const fillingIngredients = true;
+  const addRecipeNutrition = true;
 
-const fillingIngredients = true;
-const addRecipeNutrition = true;
+  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${userSearch}&maxCalories=${maxCalories}&number=${numberOfResults}&fillingredents=${fillingIngredients}&addRecipeNutrition=${addRecipeNutrition}&apiKey=${apiKey}`;
+
+  let fetchedData = [];
+  let recipeInfo = [];
+  let nutritionInformationRaw = [];
+
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      fetchedData = data.results;
+      recipeInfo = fetchedData.map(recipe => ({
+        id: recipe.id,
+        title: recipe.title,
+        image: recipe.image,
+        ingredients: recipe.nutrition.ingredients,
+        nutrition: recipe.nutrition.nutrients.slice(0, 9)
+      }));
+
+      console.log(recipeInfo[0].nutrition);
+      return recipeInfo;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      throw error; // Re-throw the error for handling at the higher level if needed
+    });
+}
 
 
-const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${userSearch}&maxCalories=${maxCalories}&${minProtein}&number=${numberOfResults}&fillingredents=${fillingIngredients}&addRecipeNutrition=${addRecipeNutrition}&apiKey=${apiKey}`;
 
-let fetchedData = [];
-let recipeInfo = [];
-let nutritionInformationRaw = [];
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    fetchedData = data.results;
-    recipeInfo = fetchedData.map(recipe => ({
-      id: recipe.id,
-      title: recipe.title,
-      image: recipe.image,
-      ingredients: recipe.nutrition.ingredients,
-      nutrition: recipe.nutrition.nutrients.slice(0, 9)
-    }));
-
-    console.log(recipeInfo[0].nutrition);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
