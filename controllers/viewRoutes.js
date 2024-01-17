19d6848c1ef6998d07c4ba349f03890c52718f74
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-17 14:11:35 -0500

    adds search route to view routes

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -16,6 +16,14 @@ viewRouter.get('/dashboard', async (req, res) => {
   }
 });
 
+viewRouter.get('/search', async (req, res) => {
+  try {
+    res.render('search', { loggedIn: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 viewRouter.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
