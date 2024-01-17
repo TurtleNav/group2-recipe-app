570ece4951094e54ae23707151985d932d08da35
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-17 18:09:57 -0500

    add search by ingredients route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const recipeRoutes = require('./recipeRoutes');
 const userRoutes = require('./userRoutes');
+const searchByIngredientsRoute = require('./searchByIngredientsRoute');
 
 router.use('/recipe', recipeRoutes);
 router.use('/users', userRoutes);
+router.use('/searchByIngredients', searchByIngredientsRoute);
 
 module.exports = router;
