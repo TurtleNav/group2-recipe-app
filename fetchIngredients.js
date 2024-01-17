411cac288b410de8075db5e5328904f8e37cd055
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-17 13:03:55 -0500

    Successfully created async await function

--- fetchIngredients.js
+++ fetchIngredients.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const fetch = require('node-fetch');
 
-const ranking = 1;
 
-async function fetchRecipesbyIngredients(ingredientsList='flour,eggs,apples', numberOfRecipes=3) {
+
+async function fetchRecipesbyIngredients(ingredientsList = 'flour,eggs,apples', numberOfRecipes = 3) {
   const apiKey = process.env.API_KEY;
+  const ranking = 1; // Set ranking to 1 to maximize available ingredients
+
   const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsList}&number=${numberOfRecipes}&ranking=${ranking}&apiKey=${apiKey}`;
 
-  const recipeData = await fetch(apiUrl).then(response => response.json())
+  const recipeData = await fetch(apiUrl).then(response => response.json());
   const recipeInfo = recipeData.map(recipe => ({
     id: recipe.id,
     title: recipe.title,
@@ -16,43 +18,38 @@ async function fetchRecipesbyIngredients(ingredientsList='flour,eggs,apples', nu
     usedIngredients: recipe.usedIngredients
   }));
 
-      const nutritionRequests = recipeInfo.map(recipe => {
-        const nutritionUrl = `https://api.spoonacular.com/recipes/${recipe.id}/nutritionWidget.json?apiKey=${apiKey}`;
+  const nutritionRequests = recipeInfo.map(recipe => {
+    const nutritionUrl = `https://api.spoonacular.com/recipes/${recipe.id}/nutritionWidget.json?apiKey=${apiKey}`;
 
-        return fetch(nutritionUrl)
-          .then(response => response.json())
-          .then(nutritionData => {
-            for (const nutrient of nutritionData.nutrients) {
-              if (nutrient.name === 'Sodium') {
-                recipe.sodium = nutrient.amount;
-              } else if (nutrient.name === 'Cholesterol') {
-                recipe.cholesterol = nutrient.amount;
-              } else if (nutrient.name === 'Sugar') {
-                recipe.sugar = nutrient.amount;
-              } else if (nutrient.name === 'Calories') {
-                recipe.calories = nutrient.amount;
-              } else if (nutrient.name === 'Fat') {
-                recipe.fat = nutrient.amount;
-              } else if (nutrient.name === 'Saturated Fat') {
-                recipe.saturatedFat = nutrient.amount;
-              } else if (nutrient.name === 'Carbohydrates') {
-                recipe.carbohydrates = nutrient.amount;
-              } else if (nutrient.name === 'Protein') {
-                recipe.protein = nutrient.amount;
-              }
-            }
-          })
-          .catch(error => {
-            console.error('Error fetching nutrition data:', error);
-          });
+    return fetch(nutritionUrl)
+      .then(response => response.json())
+      .then(nutritionData => {
+        for (const nutrient of nutritionData.nutrients) {
+          if (nutrient.name === 'Sodium') {
+            recipe.sodium = nutrient.amount;
+          } else if (nutrient.name === 'Cholesterol') {
+            recipe.cholesterol = nutrient.amount;
+          } else if (nutrient.name === 'Sugar') {
+            recipe.sugar = nutrient.amount;
+          } else if (nutrient.name === 'Calories') {
+            recipe.calories = nutrient.amount;
+          } else if (nutrient.name === 'Fat') {
+            recipe.fat = nutrient.amount;
+          } else if (nutrient.name === 'Saturated Fat') {
+            recipe.saturatedFat = nutrient.amount;
+          } else if (nutrient.name === 'Carbohydrates') {
+            recipe.carbohydrates = nutrient.amount;
+          } else if (nutrient.name === 'Protein') {
+            recipe.protein = nutrient.amount;
+          }
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching nutrition data:', error);
       });
-     
-      return Promise.all(nutritionRequests).then(() => recipeInfo);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      throw error; 
-    });
+  });
+
+  return Promise.all(nutritionRequests).then(() => recipeInfo);
 }
-  
-fetchRecipesbyIngredients()
+
+console.log(fetchRecipesbyIngredients())
