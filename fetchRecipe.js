84ef139cc1175a57976348e2e9540ea7071408d4
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-17 11:20:36 -0500

    Wrapped code in a function to make it dynanic

--- fetchRecipe.js
+++ fetchRecipe.js
@@ -1,41 +1,37 @@
 require('dotenv').config();
-const apiKey  = process.env.API_KEY;
+const fetch = require('node-fetch');
 
+function fetchRecipesWithParameters(userSearch, maxCalories, numberOfResults) {
+  const apiKey = process.env.API_KEY;
+  const fillingIngredients = true;
+  const addRecipeNutrition = true;
 
-const userSearch = 'pasta';
-const maxCalories = 700;
-const numberOfResults = 1;
+  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${userSearch}&maxCalories=${maxCalories}&number=${numberOfResults}&fillingredents=${fillingIngredients}&addRecipeNutrition=${addRecipeNutrition}&apiKey=${apiKey}`;
 
-const fillingIngredients = true;
-const addRecipeNutrition = true;
+  let fetchedData = [];
+  let recipeInfo = [];
+  let nutritionInformationRaw = [];
 
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      fetchedData = data.results;
+      recipeInfo = fetchedData.map(recipe => ({
+        id: recipe.id,
+        title: recipe.title,
+        image: recipe.image,
+        ingredients: recipe.nutrition.ingredients,
+        nutrition: recipe.nutrition.nutrients.slice(0, 9)
+      }));
 
-const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${userSearch}&maxCalories=${maxCalories}&${minProtein}&number=${numberOfResults}&fillingredents=${fillingIngredients}&addRecipeNutrition=${addRecipeNutrition}&apiKey=${apiKey}`;
-
-let fetchedData = [];
-let recipeInfo = [];
-let nutritionInformationRaw = [];
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    fetchedData = data.results;
-    recipeInfo = fetchedData.map(recipe => ({
-      id: recipe.id,
-      title: recipe.title,
-      image: recipe.image,
-      ingredients: recipe.nutrition.ingredients,
-      nutrition: recipe.nutrition.nutrients.slice(0, 9)
-     
-    }))
-
-    console.log(recipeInfo[0].nutrition)
-     
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-    
-  });
+      console.log(recipeInfo[0].nutrition);
+      return recipeInfo;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      throw error; 
+    });
+}
 
 
 
