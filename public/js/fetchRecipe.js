5244dfd283c5dd9e500788c1d5c35d456ef1fed9
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-17 10:29:57 -0500

    move and format fetch.js files

--- public/js/fetchRecipe.js
+++ public/js/fetchRecipe.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
-
 const userSearch = 'pasta';
 const maxCalories = 700;
 const minProtein = 20;
@@ -10,7 +9,6 @@ const numberOfResults = 1;
 const fillingIngredients = true;
 const addRecipeNutrition = true;
 
-
 const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${userSearch}&maxCalories=${maxCalories}&${minProtein}&number=${numberOfResults}&fillingredents=${fillingIngredients}&addRecipeNutrition=${addRecipeNutrition}&apiKey=${apiKey}`;
 
 let fetchedData = [];
@@ -18,26 +16,19 @@ let recipeInfo = [];
 let nutritionInformationRaw = [];
 
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     fetchedData = data.results;
-    recipeInfo = fetchedData.map(recipe => ({
+    recipeInfo = fetchedData.map((recipe) => ({
       id: recipe.id,
       title: recipe.title,
       image: recipe.image,
       ingredients: recipe.nutrition.ingredients,
-      nutrition: recipe.nutrition.nutrients.slice(0, 9)
-     
-    }))
+      nutrition: recipe.nutrition.nutrients.slice(0, 9),
+    }));
 
-    console.log(recipeInfo[0].nutrition)
-     
+    console.log(recipeInfo[0].nutrition);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error('Error fetching data:', error);
-    
   });
-
-
-
-
