dec9ff0daf4da90e6564443e17684f71fbad1a35
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-18 12:16:56 -0500

    fix sign-up route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,11 +17,6 @@ router.post('/', async (req, res) => {
       };
       res.status(200).json({ user: dbUserData, message: 'Login Successful' });
     });
-    // req.session.save(() => {
-    //   // req.session.loggedIn = true;
-    // res.status(200).json(dbUserData);
-    // req.session.user.id = dbUserData.user_id;
-    // });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
